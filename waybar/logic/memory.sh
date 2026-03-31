89378eb3c2e7b7faed2c73fa1cfba3120a7a8b10
joaquin bastias <joaquinbastias321@gmail.com>
2026-03-31 00:44:25 -0300

    i dont know that the fuck i can do with my life, bruh

---
+++ waybar/logic/memory.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+normalIcon="" 
+hotIcon="󰀦"
+
+usage=$(free | grep Mem | awk '{printf "%.0f", ($3/$2)*100}')
+
+icon="$normalIcon"
+class=""
+if [ "$usage" -gt 50 ]; then
+  icon="$hotIcon"
+  class="hot"
+fi
+
+total=$(free -h | grep Mem | awk '{print $2}')
+used=$(free -h | grep Mem | awk '{print $3}')
+
+text="$icon ${usage}%"
+alt="RAM: ${used}/${total}"
+
+echo "{\"text\": \"$text\", \"alt\": \"$alt\", \"class\": \"$class\"}"
